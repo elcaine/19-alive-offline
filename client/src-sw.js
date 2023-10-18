39d309e3485b05a4ec59cdc8fecfd53929ee1c78
Caine Winters@meNZ <caine.winters@outlook.com>
2023-10-18 17:57:24 -0400

    Heroku push

--- client/src-sw.js
+++ client/src-sw.js
@@ -28,7 +28,7 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  ({ request }) => ['style', 'script', 'worker', 'manifest', 'embed'].includes(request.destination),
   new offlineFallback({
     cacheName: 'asset-cache',
     plugins: [
